7038fe1a8da6067ba04584d3d3d8eceb5b169f85
ise0615 <ise0615@gmail.com>
2021-03-26 09:12:27 +0900

    画像アップロード処理をNPMモジュールに切り出した

--- htdocs/resources/src/front/ts/components/Contacts/ContactCreate.tsx
+++ htdocs/resources/src/front/ts/components/Contacts/ContactCreate.tsx
@@ -11,6 +11,8 @@ import { API } from '../../utilities'
 import { API_ENDPOINT } from '../../common/constants/api'
 import { URL } from '../../common/constants/url'
 
+import ReactImageBase64 from 'react-image-base64'
+
 interface IState {
   imageBase64?: string
   fileName?: string
@@ -27,7 +29,6 @@ export class ContactCreate extends React.Component<IProps, IState> {
       fileName: '',
       imageBase64: '',
     }
-    this.handleFileChange = this.handleFileChange.bind(this)
   }
 
   handleSubmit = async values => {
@@ -42,151 +43,6 @@ export class ContactCreate extends React.Component<IProps, IState> {
       // トップ画面に戻る
       this.props.push(URL.TOP)
     }
-  }
-
-  handleFileChange = e => {
-    const maxFileSize = 10485760 // アップロード可能な最大ファイルサイズ 10BM
-    const thumbnail_width = 500 // 画像リサイズ後の横の長さの最大値
-    const thumbnail_height = 500 // 画像リサイズ後の縦の長さの最大値
-
-    // 入力チェック
-    const validate = blob => {
-      const errors: string[] = []
-      // ファイルサイズチェック
-      if (maxFileSize < blob.size) {
-        errors.push('画像ファイルのファイルサイズが最大値(' + Math.floor(maxFileSize / 1000000) + 'MB)を超えています。')
-      }
-      return errors
-    }
-
-    const errorCallback = values => {
-      this.setState({ fileErrorMessage: values[0] })
-    }
-
-    const successCallback = values => {
-      this.setState({
-        fileName: values.fileName,
-        imageBase64: values.fileData,
-      })
-    }
-
-    // 画像のリサイズ
-    const resize = function(blob, callback, errorCallback) {
-      const image = new Image()
-      const fr = new FileReader()
-      fr.onload = function(evt) {
-        // リサイズする
-        image.onload = function() {
-          let width, height
-          if (image.width > image.height) {
-            // 横長の画像は横のサイズを指定値にあわせる
-            const ratio = image.height / image.width
-            width = thumbnail_width
-            height = thumbnail_width * ratio
-          } else {
-            // 縦長の画像は縦のサイズを指定値にあわせる
-            const ratio = image.width / image.height
-            width = thumbnail_height * ratio
-            height = thumbnail_height
-          }
-          // サムネ描画用canvasのサイズを上で算出した値に変更
-          const canvas = document.createElement('canvas')
-          canvas.id = 'canvas'
-          canvas.width = width
-          canvas.height = height
-          const ctx = canvas.getContext('2d')
-          if (ctx) {
-            // canvasに既に描画されている画像をクリア
-            ctx.clearRect(0, 0, width, height)
-            // canvasにサムネイルを描画
-            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height)
-          }
-
-          // canvasからbase64画像データを取得
-          const base64 = canvas.toDataURL('image/jpeg')
-          // base64からBlobデータを作成
-          const bin = atob(base64.split('base64,')[1])
-          const len = bin.length
-          const barr = new Uint8Array(len)
-          let i = 0
-          while (i < len) {
-            barr[i] = bin.charCodeAt(i)
-            i++
-          }
-          const resizeBlob = new Blob([barr], { type: 'image/jpeg' })
-          callback({
-            fileName: blob.name,
-            ofileData: evt.target ? evt.target.result : null,
-            fileData: base64,
-            ofileSize: blob.size,
-            fileSize: resizeBlob.size,
-            fileType: resizeBlob.type,
-          })
-        }
-        image.onerror = function() {
-          errorCallback(['選択されたファイルをロードできません。'])
-        }
-        image.src = evt.target ? evt.target.result + '' : ''
-      }
-      fr.readAsDataURL(blob)
-    }
-
-    _.each(e.target.files, (file, index) => {
-      console.log(index + ': ' + file)
-
-      function getExt(filename) {
-        const pos = filename.lastIndexOf('.')
-        if (pos === -1) return ''
-        return filename.slice(pos + 1)
-      }
-      const ext = getExt(file.name).toLowerCase()
-
-      if (ext === 'heic') {
-        // HEIC対応 iphone11 以降で撮影された画像にも対応する
-        // console.log('HEIC形式の画像なのでJPEGに変換します。')
-
-        window
-          .heic2any({
-            blob: file,
-            toType: 'image/jpeg',
-            quality: 1,
-          })
-          .then(function(resultBlob) {
-            const errors = validate(resultBlob)
-            if (0 < errors.length) {
-              errorCallback(errors)
-              return
-            }
-            resize(
-              resultBlob,
-              function(res) {
-                res.fileName = file.name
-                successCallback(res)
-              },
-              function(errors) {
-                errorCallback(errors)
-                return
-              },
-            )
-          })
-      } else {
-        const errors = validate(file)
-        if (0 < errors.length) {
-          errorCallback(errors)
-          return
-        }
-        resize(
-          file,
-          function(res) {
-            successCallback(res)
-          },
-          function(errors) {
-            errorCallback(errors)
-            return
-          },
-        )
-      }
-    })
   }
 
   submit: any
@@ -219,6 +75,7 @@ export class ContactCreate extends React.Component<IProps, IState> {
                   {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
                     <Form onSubmit={handleSubmit}>
                       <CSRFToken />
+
                       <FormGroup className="form-section">
                         <div className="form-section-wrap">
                           <Label className="item-name">
@@ -382,16 +239,15 @@ export class ContactCreate extends React.Component<IProps, IState> {
                             画像を選択してください<span>任意</span>
                           </Label>
                           <div className="textarea-wrap" id="drop-zone">
-                            <p>
-                              <Input
-                                type="file"
-                                id="js-uploadImage"
-                                // multiple="multiple"
-                                accept="image/*"
-                                capture="environment"
-                                onChange={this.handleFileChange}
-                              />
-                            </p>
+                            <ReactImageBase64
+                              handleChange={data => {
+                                console.log(data)
+                                this.setState({
+                                  imageBase64: data.imageBase64,
+                                  fileName: data.fileName,
+                                })
+                              }}
+                            />
                             <div id="result">
                               {this.state.imageBase64 &&
                                 (() => {
