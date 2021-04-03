49b22f011bb087b975b76c3329b79ac4f9e9323d
ise0615 <ise0615@gmail.com>
2021-04-03 15:36:09 +0900

    immerを導入

--- htdocs/resources/src/front/ts/reducers/parts.ts
+++ htdocs/resources/src/front/ts/reducers/parts.ts
@@ -1,21 +1,26 @@
 import { Parts } from '../store/StoreTypes'
 import { PartsAppAction, SHOW_OVERLAY, HIDE_OVERLAY } from '../actions/index'
+import produce from 'immer'
 
 const initialState: Parts = {
   isShowOverlay: false,
 }
 
-export const PartsReducer = (state = initialState, action: PartsAppAction): Parts => {
-  switch (action.type) {
-    case SHOW_OVERLAY:
-      return { ...state, isShowOverlay: true }
-    case HIDE_OVERLAY:
-      return { ...state, isShowOverlay: false }
-    default:
-      return state
-  }
+export const PartsReducer = (state = initialState, action: PartsAppAction): Parts =>
+  produce(state, draft =>
+  {
+    switch (action.type) {
+      case SHOW_OVERLAY:
+        draft['isShowOverlay'] = true
+        return
+      case HIDE_OVERLAY:
+        draft['isShowOverlay'] = false
+        return
+      default:
+        return state
+    }
 
-  return state
-}
+    return state
+  })
 
 export default PartsReducer
