86681a59add5a5d6d995ebb00c2aac1b890cd428
iseyoshitaka <ise0615@gmail.com>
2021-07-18 14:01:53 +0900

    Laravel testが動いた

--- htdocs/app/Exceptions/Handler.php
+++ htdocs/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -37,5 +38,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return redirect()->guest(route('admin.login'));
+        }
+
+        return redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 }
