0d9b9630d5dca9c81c4f447f73692f7345cf70b5
iseyoshitaka <ise0615@gmail.com>
2024-04-29 11:52:55 +0900

    ソーシャルログインが動いた

--- htdocs/config/services.php
+++ htdocs/config/services.php
@@ -31,4 +31,15 @@ return [
         ],
     ],
 
+    'stripe' => [
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ],
+
 ];
