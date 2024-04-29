5cff9df0dcb9b2bc792676ad5b8e2c56f782641c
iseyoshitaka <ise0615@gmail.com>
2024-04-29 21:33:31 +0900

    Bladeのフォーマット

--- htdocs/resources/views/layouts/app_front.blade.php
+++ htdocs/resources/views/layouts/app_front.blade.php
@@ -15,12 +15,10 @@
 <body>
 <div id="react-root"></div>
 <script>
-    const laravelSession = {};
-    laravelSession['status'] = @if(session('status'))'{{session('status')}}'
-    @else''@endif;
-    laravelSession['resent'] = @if(session('resent'))'{{session('resent')}}'
-    @else''@endif;
-    const laravelErrors =@php print(htmlspecialchars_decode($errors))@endphp;
+    var laravelSession = {};
+    laravelSession['status'] = @if(session('status'))'{{session('status')}}'@else''@endif;
+    laravelSession['resent'] = @if(session('resent'))'{{session('resent')}}'@else''@endif;
+    var laravelErrors =@php print(htmlspecialchars_decode($errors))@endphp;
 </script>
 <script src="{{ asset('/assets/front/js/app.js') }}" defer></script>
 <script src="https://js.stripe.com/v3/"></script>
