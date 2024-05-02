6bdbc91aad08affff74c33c96ca6d9f928ded07f
iseyoshitaka <ise0615@gmail.com>
2024-05-02 22:23:29 +0900

    ContactFormのテストコードを追加

--- htdocs/app/Domain/Entities/ContactFormImage.php
+++ htdocs/app/Domain/Entities/ContactFormImage.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Entities;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -15,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class ContactFormImage extends Model
 {
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
--- htdocs/app/Domain/Repositories/ContactForm/ContactFormImageRepository.php
+++ htdocs/app/Domain/Repositories/ContactForm/ContactFormImageRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Domain\Repositories\ContactForm;
 
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 use App\Domain\Repositories\BaseRepository;
 
