ba9e9787fe4590e7bcf7751eb6c5139465d48372
iseyoshitaka <ise0615@gmail.com>
2025-12-31 17:10:28 +0900

    コード整形

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use App\Http\Middleware\AccessControlMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
