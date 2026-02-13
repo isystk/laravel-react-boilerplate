401df37f2e93fc659472e7a2cbb0d0265fb417c1
iseyoshitaka <ise0615@gmail.com>
2026-02-14 00:42:33 +0900

    [feature/test_code] feat: Googleログイン対応に伴うユーザーテーブルのスキーマ変更

--- database/migrations/2026_01_01_000000_create_users_table.php
+++ database/migrations/2026_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->dateTime('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable()->unique();
+            $table->string('avatar_url')->nullable();
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
         });
